be172aaefccac65fb59741b7c5b5df5706479ed0
Samuel Gabriel Sousa <emanuel.gabriel.sousa@protonmail.com>
2021-02-10 21:32:24 -0300

    criação da tabela de turma

---
+++ criar-tabela-turma.sql
@@ -0,0 +1,6 @@
+INSERT INTO privado.turma(
+	valor_turma, desconto, data_inicio, data_termino, id_aluno, id_curso)
+VALUES (420.10, 1.0, '2021-02-08 19:00:00', '2021-05-10 20:00:00', 9, 1);
+
+SELECT * FROM privado.turma
+SELECT * FROM privado.aluno
